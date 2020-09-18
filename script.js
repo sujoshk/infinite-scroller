aad5e3bc10b4ea4490621616ebac48789a442df9
Sujosh <sujoshk@gmail.com>
2020-09-18 12:26:31 +0530

    made the code dry

--- script.js
+++ script.js
@@ -16,18 +16,45 @@ const apiURL = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&coun
 
 // Create elements for links and photos, add that to the dom
 
+
+// Helper function to set attributes on DOM elements
+
+function setAttributes(element, attributes) {
+    for(const key in attributes) {
+        element.setAttribute(key, attributes[key]);
+    }
+}
+
+
+
 function displayPhotos() {
     // Run function for each object in the photosArray
     photosArray.forEach((photo) => {
         // create <a> to link to unsplash
         const item = document.createElement('a');
-        item.setAttribute('href', photo.links.html);
-        item.setAttribute('target', '_blank');
+
+        setAttributes(item, {
+            href: photo.links.html,
+            target: '_blank',
+        })
+
+        // item.setAttribute('href', photo.links.html);
+        // item.setAttribute('target', '_blank');
+
+
         // create <img> for photo
         const img = document.createElement('img');
-        img.setAttribute('src', photo.urls.regular );
-        img.setAttribute('alt', photo.alt_description);
-        img.setAttribute('title', photo.alt_description);
+
+
+        // img.setAttribute('src', photo.urls.regular );
+        // img.setAttribute('alt', photo.alt_description);
+        // img.setAttribute('title', photo.alt_description);
+
+        setAttributes(img, {
+            src: photo.urls.regular,
+            alt: photo.alt_description,
+            title: photo.alt_description,
+        });
 
         // put <img> inside <a>, then put both inside image container element
 
